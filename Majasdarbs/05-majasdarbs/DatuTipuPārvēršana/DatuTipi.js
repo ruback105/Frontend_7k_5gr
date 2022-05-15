f2f83eccc0f98df89ea2c0c9f2f0090d5f82cb71
Edvards Safronovs <edwardosafr@gmail.com>
2022-05-15 11:45:07 +0300

    Datu tipi

--- Majasdarbs/05-majasdarbs/DatuTipuPārvēršana/DatuTipi.js
+++ Majasdarbs/05-majasdarbs/DatuTipuPārvēršana/DatuTipi.js
@@ -4,44 +4,44 @@
 
 
 String(123)            //"123"
-String(-12.3)       
-String(null)         
-String(undefined)       
-String(true)          
-String(false)        
-String(function () {})  
-String({})             
-String({ key: 42 })     
-String([])              
-String([1, 2])          
+String(-12.3)       //"-12.3"
+String(null)           //"null"
+String(undefined)       //"undefinied"
+String(true)          //"true"
+String(false)        //"false"
+String(function () {})  // "'function () {}'"
+String({})             //"'[object Object]'"
+String({ key: 42 })    //"'[object Object]'" 
+String([])              //""
+String([1, 2])        // "1,2"  
 
 
-Number("123")           
-Number("123.4")        
-Number("123,4")         
-Number("")              
-Number(null)            
-Number(undefined)       
-Number(true)            
-Number(false)           
-Number(function () {})  
-Number({})              
-Number([])              
-Number([1])             
-Number([1, 2])          
+Number("123")           //123
+Number("123.4")        //123.4
+Number("123,4")        //NaN
+Number("")              //0
+Number(null)            //0
+Number(undefined)       //NaN
+Number(true)            //1 
+Number(false)           //0
+Number(function () {})  //NaN
+Number({})              //NaN
+Number([])              //0
+Number([1])             //1
+Number([1, 2])          //NaN
 
 
-Boolean("")             
-Boolean("string")       
-Boolean("false")        
-Boolean(0)              
-Boolean(42)             
-Boolean(-42)            
-Boolean(NaN)            
-Boolean(null)           
-Boolean(undefined)      
-Boolean(function () {}) 
-Boolean({})             
-Boolean({ key: 42 })    
-Boolean([])             
-Boolean([1, 2])     
+Boolean("")   //false          
+Boolean("string")      //true 
+Boolean("false")        //true
+Boolean(0)              //false
+Boolean(42)             //true
+Boolean(-42)            //true
+Boolean(NaN)            ///false
+Boolean(null)           //false
+Boolean(undefined)      //false
+Boolean(function () {}) //true
+Boolean({})             //true
+Boolean({ key: 42 })    //true
+Boolean([])             //true
+Boolean([1, 2])     //true

fa09bfd582c02e1bf3c6e5c2272184428d19bb51
Janis1979-byte <janis.ruselis@inbox.lv>
2022-05-15 15:43:13 +0300

    05majasdarbs

--- Majasdarbs/05-majasdarbs/Object/cv.js
+++ Majasdarbs/05-majasdarbs/Object/cv.js
@@ -1,3 +1,20 @@
 const VardsUzvards = {
-    
-}
+firstName: "Dina",
+lastName: "Rusele",
+age: 30,
+experienece: {
+    jobs: [
+        "Company1",
+        "Company2"
+        ],
+    languages: [
+        "Latvian",
+        "English"
+        ],
+    education: [
+        "Latvian Univercity of Agriculture",
+        " Latvian University"
+    ],
+skills: ["Project Management, AutoCAD"],
+        }
+};

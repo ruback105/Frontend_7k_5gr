1e4778cf6655bcc75d4a4a0f298a1306414e59e3
Edvards Safronovs <edwardosafr@gmail.com>
2022-05-15 12:36:43 +0300

    Objekts

--- Majasdarbs/05-majasdarbs/Object/cv.js
+++ Majasdarbs/05-majasdarbs/Object/cv.js
@@ -1,3 +1,27 @@
-const VardsUzvards = {
-    
+const cilveks = {
+    firstName: "Edvards",
+    lastName: "Safronovs",
+    age: 26,
+   
+    skills: {
+        LanguagesSpoken: [
+            "Russian",
+            "Latvian",
+            "English"
+        ],
+        
+    Web:["HTML, CSS, React JS, Bootstrap"],
+
+    },
+
+    Workexperience: "Tele2, Tele3",
+    Education: "Secondary SChool, Latvian Univercity",
+
+    FAQ:{
+        MyHobbies:["Cycling, Diving"],
+        MyFavouritePlaylist:"open.spotify.com/embed/playlist/37i9dQZF1DXcBWIGoYBM5M?utm_source=generator",
+    }
 }
+
+console.log("Person full name:", cilveks.firstName + cilveks.lastName); 
+console.log(`Age of ${cilveks.firstName} ${cilveks.lastName}:`, cilveks.age);

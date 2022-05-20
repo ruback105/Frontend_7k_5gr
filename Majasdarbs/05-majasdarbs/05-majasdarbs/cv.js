b8417ac3902939d5d342ee8bbe50906121f56502
mrMartinsRents <martinsrents@gmail.com>
2022-05-20 12:01:24 +0300

    05-majasdarbs

---
+++ Majasdarbs/05-majasdarbs/05-majasdarbs/cv.js
@@ -0,0 +1,25 @@
+const VardsUzvards = {
+    firstName: "Matt",
+    lastName: "Developer",
+    age: 42,
+    experienece: {
+        jobs: [
+            "Apple",
+            "Google",
+            "Amazon"
+            ],
+        languages: [
+            "Latvian",
+            "English",
+            "Mandarin"
+            ],
+        education: [
+            "MIT",
+            "Harvard"
+        ],
+    skills: [
+        "Front-end Developer",
+        "Designer"
+    ],
+            }
+    };

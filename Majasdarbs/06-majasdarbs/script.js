2be5f441273873cc728636a8916541bcc1d6aaaa
Arturs Pusko <arturs.pusko@gmail.com>
2022-05-22 10:47:40 +0300

    add 6th lesson

---
+++ Majasdarbs/06-majasdarbs/script.js
@@ -0,0 +1,39 @@
+/*  
+    Aplikacijas darba principu var apskatities video
+    success-case.mp4
+    bad-case.mp4
+
+    Aplikacijas darba princips:
+        Mums piejajams masivs ar burtiem
+        Lietotajam ir iespeja ievadit skaitli 
+        Ja ievaditais skaitlis atbilst burtu masivam tad šis burts tiek izvadits uz ekranu 
+        Spiežot pogas uz tastaturas aplikacija parbauda ievadito vertibu ar izveleto burtu un pažiņo par rezultatu
+*/
+
+
+// Implementacija
+// 1) izvejdot massivu ar burtiem characters[]
+
+// 2) izvejtot mainigo "arrayId" šis mainigas saņems lietotaja ievadito skaitli
+
+// 3) izvejtot mainigo "output" šis mainigas satur DOM elementu kur tiek izvadits izveletais burts (getElementById) 
+
+/* 4) izvejtot funkciju getValue() funkcijas ipašibas 
+        neko nesaņem
+        izmantojot prompt() medoti --> https://www.w3schools.com/jsref/met_win_prompt.asp piesaistit vertibu mainigam arrayId
+        izveidot parbaudi if else un parliecinaties ka ievietotais skatlis pieder intervalam no 0 lidz characters.lenght
+        ja gadijuma izsaukt finkciju print value (5 punkts)
+        ne gadijuma izvadit kļudas paziņojomu pec tam izsaukt funkciju getValue velreizi
+*/      
+
+/* 5) izvejtot funkciju printValues() funkcijas ipašibas
+        neko nesaņem
+        izmaiņam defineto output mainigo iekšejo text (innerText) ar izveleto burtu masiva (characters[arrayId])
+*/
+
+// 6) izsaucam funkciju getValue()
+
+/* 7) japievino event listener keyPress un parbaudit ievadito to tastaturas vertibu ar izveleto vertibu pirm tam
+        ja gadijuma izvadit paziņojumu alert('PAREIZI') un izsaukt funkciju getValue() vel reizi
+        ne gadijum izvadit paziņojumu alert('NEPAREIZI')
+*/      
---
+++ course/06-JS-CYCLES-FC-EVENTS/index.js
@@ -0,0 +1,49 @@
+// const numberArray = [
+//   3.1245124, 3.14, 5.31523, 324.546456, 0.3245234534, 6.23425, 10000.3242526,
+//   9.999999, 10,
+// ];
+
+// const fixedNumberArray = [];
+
+// fixedNumberArray[0] = numberArray[0].toFixed(2);
+// fixedNumberArray[1] = numberArray[1].toFixed(2);
+// fixedNumberArray[2] = numberArray[2].toFixed(2);
+// fixedNumberArray[3] = numberArray[3].toFixed(2);
+
+// for (let numberIndex = 0; numberIndex < numberArray.length; numberIndex++) {
+//   //   console.log(`current index: ${numberIndex}  -  current item: ${numberArray[numberIndex]}`);
+
+//   if (numberArray[numberIndex] >= 10) {
+//     continue;
+//   }
+
+//   fixedNumberArray.push(numberArray[numberIndex].toFixed(2));
+//   lots of code
+// }
+
+// numberArray.forEach((item) => {
+//   if (item < 10) {
+//     fixedNumberArray.push(item.toFixed(2));
+//   }
+//   // lots of code
+// });
+
+// console.log(fixedNumberArray);
+
+const emails = [
+  "  test@Email.com ",
+  "new_email@Gmail.com  ",
+  "TesT@email.com  ",
+  "  hello@world.com  ",
+  "  hello1@woRld.com  ",
+  "  hello2@woRld.com  ",
+  "  hello3@woRld.com  ",
+  "  hello4@woRld.com  ",
+];
+
+const formattedEmails = [];
+
+// const firstEmail = emails[0].toLocaleLowerCase().trim();
+// formattedEmails.push(firstEmail);
+
+console.log(formattedEmails); //

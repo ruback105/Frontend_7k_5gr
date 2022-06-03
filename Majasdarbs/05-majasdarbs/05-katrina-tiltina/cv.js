9a26f75bd20c3f8f28555b58991b2d94a8f12e14
Katrina Tiltina <katrina.tiltin@gmail.com>
2022-06-03 13:32:06 +0300

    05 mājasdarbi katrina tiltina

---
+++ Majasdarbs/05-majasdarbs/05-katrina-tiltina/cv.js
@@ -0,0 +1,31 @@
+const katrinaTiltinaCV = {
+    firstName: "Katrina",
+    lastName: "Tiltina",
+    age: 33,
+    education: [
+        "NDG",
+        "LU"
+    ],
+    jobExperience: {
+        linusmedical: {
+            company: "Linus Medical",
+            role: "office manager",
+            timePeriod: 3
+        },
+        jaunsardzescntrs: {
+            company: "Jaunsardzes centrs",
+            role: "PR specialist",
+            timePeriod: 4
+        }
+    },
+    languages: [
+        "latvian",
+        "english",
+        "russian"
+    ],
+    hobbies: [
+        "snowboarding",
+        "dancing",
+        "front end developing"
+    ]
+}

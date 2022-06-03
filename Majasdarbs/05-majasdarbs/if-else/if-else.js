13a47c9c6756e004bba0a632efbb4002bcc21624
Arturs Pusko <arturs.pusko@gmail.com>
2022-06-03 18:22:12 +0300

    add homework solution

--- Majasdarbs/05-majasdarbs/if-else/if-else.js
+++ Majasdarbs/05-majasdarbs/if-else/if-else.js
@@ -1,17 +1,45 @@
-if (darbaDiena === 'Pirmdiena') {
-    console.log('1');
-} else if (darbaDiena === 'Otrdiena') {
-    console.log('2');
-} else if (darbaDiena === 'Trešdiena') {
-    console.log('3');
-} else if (darbaDiena === 'Ceturtdiena') {
-    console.log('4');
-} else if (darbaDiena === 'Piektdiena') {
-    console.log('5');
-} else if (darbaDiena === 'Sestdiena') {
-    console.log('6');
-} else if (darbaDiena === 'Svētdiena') {
-    console.log('7');
-} else {
-    console.log('Error');
+const darbaDiena = "Otrdiena";
+
+// if (darbaDiena === "Pirmdiena") {
+//   console.log("1");
+// } else if (darbaDiena === "Otrdiena") {
+//   console.log("2");
+// } else if (darbaDiena === "Trešdiena") {
+//   console.log("3");
+// } else if (darbaDiena === "Ceturtdiena") {
+//   console.log("4");
+// } else if (darbaDiena === "Piektdiena") {
+//   console.log("5");
+// } else if (darbaDiena === "Sestdiena") {
+//   console.log("6");
+// } else if (darbaDiena === "Svētdiena") {
+//   console.log("7");
+// } else {
+//   console.log("Error");
+// }
+
+switch (darbaDiena) {
+  case "Pirmdiena":
+    console.log("1");
+    return;
+  case "Otrdiena":
+    console.log("2");
+    return;
+  case "Trešdiena":
+    console.log("3");
+    return;
+  case "Ceturtdiena":
+    console.log("4");
+    return;
+  case "Piektdiena":
+    console.log("5");
+    return;
+  case "Sestdiena":
+    console.log("6");
+    return;
+  case "Svētdiena":
+    console.log("7");
+    return;
+  default:
+    return;
 }

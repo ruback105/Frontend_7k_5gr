0792c78406f19b95cb0169c32c301110296d3b4a
Edvards Safronovs <edwardosafr@gmail.com>
2022-06-04 14:47:00 +0300

    6 md

--- Majasdarbs/06-majasdarbs/script.js
+++ Majasdarbs/06-majasdarbs/script.js
@@ -11,13 +11,20 @@
 */
 
 
+const inputIndex = prompt("Enter number");
+
 // Implementacija
 // 1) izvejdot massivu ar burtiem characters[]
 
+const characters = ["a", "b", "c", "d", "e", "f"];
+
 // 2) izvejtot mainigo "arrayId" šis mainigas saņems lietotaja ievadito skaitli
 
+let arrayId = prompt("Enter number");
+console.log(characters[arrayId]);
 // 3) izvejtot mainigo "output" šis mainigas satur DOM elementu kur tiek izvadits izveletais burts (getElementById) 
 
+const output = document.getElementById();
 /* 4) izvejtot funkciju getValue() funkcijas ipašibas 
         neko nesaņem
         izmantojot prompt() medoti --> https://www.w3schools.com/jsref/met_win_prompt.asp piesaistit vertibu mainigam arrayId
@@ -25,11 +32,29 @@
         ja gadijuma izsaukt finkciju print value (5 punkts)
         ne gadijuma izvadit kļudas paziņojomu pec tam izsaukt funkciju getValue velreizi
 */      
+const getValue = () => {
+
+let arrayId = prompt(1,2);
+
+if(arrayId  >= 0 && arrayId <= characters.length){
+const ja = console.log(print.value);
+
+}
+
+else{
+
+
+}
+
+}
+
+
 
 /* 5) izvejtot funkciju printValues() funkcijas ipašibas
         neko nesaņem
         izmaiņam defineto output mainigo iekšejo text (innerText) ar izveleto burtu masiva (characters[arrayId])
 */
+
 
 // 6) izsaucam funkciju getValue()
 
@@ -37,3 +62,17 @@
         ja gadijuma izvadit paziņojumu alert('PAREIZI') un izsaukt funkciju getValue() vel reizi
         ne gadijum izvadit paziņojumu alert('NEPAREIZI')
 */      
+
+document.addEventListener("keyup",(evt)=> {
+if(evt.key===characters[arrayId]){
+        console.log("SUCCESS");
+
+}
+
+else{
+
+        console.log("NEPAREIZI")
+}
+
+
+})

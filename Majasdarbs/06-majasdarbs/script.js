89951159044dedae485b0c7d1c210349cd85b42d
artursslisans <arturs.slisans1@gmail.com>
2022-06-07 18:33:36 +0300

    06-Arturs SLisans

--- Majasdarbs/06-majasdarbs/script.js
+++ Majasdarbs/06-majasdarbs/script.js
@@ -14,9 +14,13 @@
 // Implementacija
 // 1) izvejdot massivu ar burtiem characters[]
 
+const characters ["a","b","c","d","e","l","o","j","z"];
+
 // 2) izvejtot mainigo "arrayId" šis mainigas saņems lietotaja ievadito skaitli
+let arrayId; 
 
 // 3) izvejtot mainigo "output" šis mainigas satur DOM elementu kur tiek izvadits izveletais burts (getElementById) 
+const output=document.getElementById("main");
 
 /* 4) izvejtot funkciju getValue() funkcijas ipašibas 
         neko nesaņem
@@ -26,14 +30,39 @@
         ne gadijuma izvadit kļudas paziņojomu pec tam izsaukt funkciju getValue velreizi
 */      
 
+const getValue=()=>{
+        prompt( Enter number from 0 to ${characters.lenght-1});
+
+        if(arrayId>=0 && arrayId <= characters.lenght) {
+
+        }else{
+                getValue();
+        }
+};
+
+getValue();
+
+
+
 /* 5) izvejtot funkciju printValues() funkcijas ipašibas
         neko nesaņem
         izmaiņam defineto output mainigo iekšejo text (innerText) ar izveleto burtu masiva (characters[arrayId])
 */
+const printValues=()=>{
+        output.innerText=characters[]
+};
 
 // 6) izsaucam funkciju getValue()
+
+getValue();
+
+
+
 
 /* 7) japievino event listener keyPress un parbaudit ievadito to tastaturas vertibu ar izveleto vertibu pirm tam
         ja gadijuma izvadit paziņojumu alert('PAREIZI') un izsaukt funkciju getValue() vel reizi
         ne gadijum izvadit paziņojumu alert('NEPAREIZI')
 */      
+window.addEventListener("keypress",(e)=> {
+        console.log(e.key);
+});

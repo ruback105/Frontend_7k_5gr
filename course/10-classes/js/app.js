ede9beef5263dfd82315091b6359658bf2138d6e
Arturs Pusko <arturs.pusko@gmail.com>
2022-06-19 11:09:14 +0300

    add class example

---
+++ course/10-classes/js/app.js
@@ -0,0 +1,100 @@
+// const person1 = {
+//   name: "Arturs",
+//   age: 23,
+//   email: "arturs@gmail.com",
+//   getInfo: function () {
+//     return `Hey, my name is ${this.name} I'm ${this.age} old. Contact me via ${this.email}`;
+//   },
+// };
+
+// const person2 = {
+//   name: "Janis",
+//   age: 30,
+//   email: "janis@gmail.com",
+//   getInfo: function () {
+//     return `Hey, my name is ${this.name} I'm ${this.age} old. Contact me via ${this.email}`;
+//   },
+// };
+
+// const person3 = {
+//   name: "Tatjana",
+//   age: 35,
+//   email: "tatjana@gmail.com",
+//   getInfo: function () {
+//     return `Hey, my name is ${this.name} I'm ${this.age} old. Contact me via ${this.email}`;
+//   },
+// };
+
+// person1.getInfo();
+// person2.getInfo();
+// person3.getInfo();
+
+// class Person {
+//   constructor(name, age, email) {
+//     this.name = name;
+//     this.age = age;
+//     this.email = email;
+
+//     this.info = this.getInfo();
+//   }
+
+//   getInfo() {
+//     return `Hey, my name is ${this.name} I'm ${this.age} old. Contact me via ${this.email}`;
+//   }
+// }
+
+// const person1 = new Person("Arturs", 23, "arturs@gmail.com");
+// const person2 = new Person("Janis", 30, "janis@gmail.com");
+// const person3 = new Person("Tatjana", 35, "tatjana@gmail.com");
+
+// console.log(person1.info);
+// console.log(person2.info);
+// console.log(person3.info);
+
+// class Car {
+//   constructor(name, model, fuelType) {
+//     this.name = name;
+//     this.model = model;
+//     this.fuelType = fuelType;
+//   }
+
+//   getInfo() {
+//     return `My car is ${this.name} ${this.model} - ${this.fuelType}`;
+//   }
+// }
+
+// class Audi {
+//   constructor(model, fuelType) {
+//     this.name = "Audi";
+//     this.model = model;
+//     this.fuelType = fuelType;
+//   }
+
+//   getInfo() {
+//     return `My car is ${this.name} ${this.model} - ${this.fuelType}`;
+//   }
+// }
+
+// class Tesla {
+//     constructor(model, fuelType) {
+//       this.name = "Tesla";
+//       this.model = model;
+//       this.fuelType = fuelType;
+//     }
+
+//     getInfo() {
+//       return `My car is ${this.name} ${this.model} - ${this.fuelType}`;
+//     }
+//   }
+
+// const audiA3 = new Car("audi", "a3", "diesel");
+
+// console.log(audiA3.getInfo());
+
+// uztaisīt Animal klasi ar parametriem: dzīvnieks, name, age, weight
+
+// const cat = new Animal("cat", "Minka", 3, 7);
+// const dog = new Animal("dog", "Rekss", 5, 20);
+
+// console.log(cat.getInfo());
+// console.log(dog.getInfo());
